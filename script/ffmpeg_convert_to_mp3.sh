2390b453375bb071954823e60b4c884ed4fdd271
johannli <johannli@tencent.com>
2023-03-07 11:26:48 +0800

    :mute: remove ffmpeg log, only output error log

--- script/ffmpeg_convert_to_mp3.sh
+++ script/ffmpeg_convert_to_mp3.sh
@@ -4,38 +4,39 @@
 
 # Check if mp3 directory exists, if not, create it
 if [ ! -d "mp3" ]; then
-    mkdir mp3
+  mkdir mp3
 fi
 
 # Iterate over all mp4 files in audio directory, and convert them to mp3
 for audio_file in ./audio/*; do
-    filename=$(basename "$audio_file")
-    # Check if a mp3 file with the same name already exists in mp3 directory, if so, output a message and skip
-    if [ -f "./mp3/${filename%.*}.mp3" ]; then
-        echo "File ./mp3/${filename%.*}.mp3 already exists, skipping ..."
-        continue
-    fi
+  filename=$(basename "$audio_file")
+  # Check if a mp3 file with the same name already exists in mp3 directory, if so, output a message and skip
+  if [ -f "./mp3/${filename%.*}.mp3" ]; then
+    echo "File ./mp3/${filename%.*}.mp3 already exists, skipping ..."
+    continue
+  fi
 
-    filename_without_extension="${filename%.*}"
-    output_file="./mp3/${filename_without_extension}.mp3"
+  filename_without_extension="${filename%.*}"
+  output_file="./mp3/${filename_without_extension}.mp3"
 
-    echo "Converting $audio_file to $output_file ..."
+  echo "Converting $audio_file to $output_file ..."
 
-    # ffmpeg 
-    # -i: Specifies input file name;
-    # -vn: Only extract audio stream, without audio stream;
-    # -acodec libmp3lame: Set audio encoder to libmp3lame;
-    # -ar 44100: Set audio sampling rate to 44100 Hz;
-    # -ab 192k: Set audio bitrate to 128 kbps;
-    # -f mp3: Set output format to MP3.
+  # ffmpeg
+  # -i: Specifies input file name;
+  # -vn: Only extract audio stream, without audio stream;
+  # -acodec libmp3lame: Set audio encoder to libmp3lame;
+  # -ar 44100: Set audio sampling rate to 44100 Hz;
+  # -ab 192k: Set audio bitrate to 128 kbps;
+  # -f mp3: Set output format to MP3.
+  # -loglevel error: Only output error messages.
 
-    ffmpeg -i "$audio_file" -vn -acodec libmp3lame -ab 128k -ar 44100 "$output_file" 2>&1
-    if [[ "$ffmpeg_output" == *"Invalid data found when processing input"* ]]; then
-      # The file cannot be converted to MP3
-      echo "The file $input_file cannot be converted to MP3"
-    else
-      # The file can be converted to MP3
-      echo "The file $input_file can be converted to MP3"
-    fi
+  ffmpeg -i "$audio_file" -vn -acodec libmp3lame -ab 128k -ar 44100 -loglevel error"$output_file" 2>&1
+  if [[ "$ffmpeg_output" == *"Invalid data found when processing input"* ]]; then
+    # The file cannot be converted to MP3
+    echo "The file $input_file cannot be converted to MP3"
+  else
+    # The file can be converted to MP3
+    echo "The file $input_file can be converted to MP3"
+  fi
 
 done
